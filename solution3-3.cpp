644ebf9f183c528a5cc077ce70e284765a52d4d2
Ali Butt <alitbutt@users.noreply.github.com>
2021-03-29 02:16:18 -0700

    Create solution3-3.cpp

---
+++ solution3-3.cpp
@@ -0,0 +1,60 @@
+/*
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
+For example, given:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+Copyright 2009–2021 by Codility Limited. All Rights Reserved.
+
+Result: 
+*/
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> &A) {
+    int i=0, j=A.size()-1;
+    int si=A[i], sj=A[j];
+    while (i < j-1)
+        if (abs((si+A[i+1])-sj) < abs(si-(sj+A[j-1])))
+            si += A[++i];
+        else
+            sj += A[--j];
+
+    return abs(si - sj);
+}
